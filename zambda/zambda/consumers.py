234ecbb6e794957e5ee80631724f97888b0c862d
Your Name <you@example.com>
2020-06-09 01:20:52 +0200

    Testing messaging system

--- zambda/zambda/consumers.py
+++ zambda/zambda/consumers.py
@@ -8,38 +8,36 @@ import json
 class ChatConsumer(websocket.JsonWebsocketConsumer):
     def connect(self):
         # Join the thread
-        thread_reference = self.scope['url_root']['kwargs']['reference']
+        thread_reference = self.scope['url_route']['kwargs']['reference']
         self.thread_name = f'thread_{thread_reference}'
 
-        print(thread_reference)
-
-        # async_to_sync(self.channel_layer.group_add)(
-        #     self.thread_name,
-        #     self.channel_name
-        # )
+        async_to_sync(self.channel_layer.group_add)(
+             self.thread_name,
+             self.channel_name
+        )
 
         self.accept()
 
     def disconnect(self, code):
         # Leave thread
-        # async_to_sync(self.channel_layer.group_discard)(
-        #     self.thread_name,
-        #     self.channel_name
-        # )
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.thread_name,
+            self.channel_name
+        )
 
     def receive(self, text_data):
-        # message = 'My message'
-        # Send message to thread
-        # async_to_sync(self.channel_layer.group_send)(
-        #     self.thread_name,
-        #     {
-        #         'type': 'chat_message',
-        #         'message': message
-        #     }
-        # )
+        message = 'Hi this is my first message on the forum'
+        
+	#Send message to thread
+        async_to_sync(self.channel_layer.group_send)(
+            self.thread_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
 
-        # self.send(text_data="We are a connected")
+        self.send(text_data="We are a connected")
         self.close(code=1000)
 
     def chat_message(self, event):
